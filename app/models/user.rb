fb2c9d3e3297bf632583d6b2b76776de3165cd72
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-02-16 22:51:31 +0200

    remove confirmable email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true
-
-  has_many :foods
-  has_many :recipes
 end
