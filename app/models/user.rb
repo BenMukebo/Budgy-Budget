993448c683df038919fd0a804002348d49550471
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-02-17 00:56:21 +0200

    Generate migration files and schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
+  has_many :categories
 end
