9bace1e6aff6e589ab55977dd33e5c3c96e0da30
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-02-17 15:49:53 +0200

    Give style to the edit category view page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,7 +7,9 @@ class CategoriesController < ApplicationController
   end
 
   # GET /categories/1 or /categories/1.json
-  def show; end
+  def show
+    @category = set_category
+  end
 
   # GET /categories/new
   def new
