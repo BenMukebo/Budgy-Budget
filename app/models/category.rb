e1ba5ebaf4d5bd3ca77a171599f2b01e357245bd
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-02-18 17:02:56 +0200

    Create total amount method that display the total transaction amount from a category

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,8 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :category_records, dependent: :delete_all
   has_many :records, through: :category_records, dependent: :delete_all
+
+  def total_amount
+    records.sum('amount')
+  end
 end
