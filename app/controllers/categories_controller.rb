7adebbd2aff4a573028ff1b83a6b8738d5b35cae
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-02-19 10:26:23 +0200

    Make last tach

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -31,7 +31,7 @@ class CategoriesController < ApplicationController
     respond_to do |format|
       if @category.save
         # format.html { redirect_to category_url(@category), notice: 'Category was successfully created.' }
-        format.html { redirect_to categories_path notice: 'Category was successfully created.' }
+        format.html { redirect_to categories_path, notice: 'Category was successfully created.' }
 
       else
         format.html { render :new, status: :unprocessable_entity }
